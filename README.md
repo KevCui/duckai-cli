4ce55ff82c3da79c5c5a37e38963bbffbd84194a
Kevin Cui <krazycavin@gmail.com>
2024-11-11 20:43:29 +0100

    Add main script

---
+++ README.md
@@ -0,0 +1,41 @@
+# DuckAI-cli
+
+> Chat with [DuckDuckGo AI](https://duck.ai/) in terminal
+
+# Table of Contents
+
+- [Dependency](#dependency)
+- [Installation](#installation)
+- [How to use](#how-to-use)
+- [Want an alternative?](#want-an-alternative)
+- [Note](#note)
+
+## Dependency
+
+- [playwright-chromium](https://github.com/Microsoft/playwright)
+
+## Installation
+
+```bash
+npm i playwright-chromium
+npx playwright install chromium
+```
+
+## How to use
+
+```bash
+$ ./duckai.js "enter any text here"
+```
+
+## Want an alternative?
+
+- Check out [chatpgt-cli](https://github.com/KevCui/chatgpt-cli)
+- Check out [copilot-cli](https://github.com/KevCui/copilot-cli)
+
+## Note
+
+This script is designed to process one question and answer at a time, delivering in plain text format. It is optimized for command-line use, enabling users to rapidly retrieve answers directly in the terminal, without having to engage in a conversation with DuckDuckGo AI.
+
+---
+
+<a href="https://www.buymeacoffee.com/kevcui" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-orange.png" alt="Buy Me A Coffee" height="60px" width="217px"></a>
---
+++ duckai.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const { chromium } = require('playwright-extra');
+
+const searchText = process.argv[2];
+const url = 'https://duck.ai';
+const textareaSearchBox = '[name="user-prompt"]';
+const buttonSubmit = '[aria-label="Send"]';
+const textMessage = '.JXNYs5FNOplxLlVAOswQ';
+const totalLoopCount = 60;
+const printIntervalTime = 1000;
+const cookie = {
+  name: 'dcm',
+  value: '3', // 3: GPT-4o mini; 1: Haiku; 5: Llama; 6: Mixtral
+  domain: 'duckduckgo.com',
+  path: '/'
+};
+
+chromium.launch({ headless: false, timeout: 10000 }).then(async browser => {
+  // Set page
+  const context = await browser.newContext();
+  await context.addCookies([cookie]);
+  const page = await context.newPage();
+
+  // Start page
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+  // Submit question
+  await page.fill(textareaSearchBox, searchText);
+  await page.click(buttonSubmit);
+
+  // Get reply
+  var prevResult = '';
+  for (let i = 0; i < totalLoopCount; i++) {
+    await new Promise((resolve) => setTimeout(resolve, printIntervalTime));
+
+    var result = await page.locator(textMessage).innerHTML();
+    // Remove <p>
+    result = result.replace(/<p>/g, '');
+    // Convert <li> to -
+    result = result.replace(/<li>/g, '- ');
+    // Remove remaining HTML tags
+    result = result.replace(/<\/?[^>]+(>|$)/g, "");
+
+    // Decode HTML using temporary element
+    const decodedResult = await page.evaluate(encoded => {
+        const textarea = document.createElement('textarea');
+        textarea.innerHTML = encoded;
+        return textarea.value;
+    }, result);
+
+    console.clear();
+    console.log('----------\n' + decodedResult);
+    if (prevResult == result && i != (totalLoopCount - 1)) {
+      i = (totalLoopCount - 1);
+    }
+    prevResult = result
+  }
+
+  // Close browser
+  await browser.close();
+});
